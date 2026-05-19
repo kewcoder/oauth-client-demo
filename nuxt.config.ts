70d09ce88271bafcb71f6f69e0cdb1cb28c71615
Ari Bahtiar <kewcoder@gmail.com>
2026-05-19 18:29:26 +0700

    only for dev

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,18 +4,16 @@ export default defineNuxtConfig({
   compatibilityDate: '2024-12-01',
   devtools: { enabled: true },
   devServer: {
-    https: {
-      key: readFileSync('./certs/localhost-key.pem', 'utf-8'),
-      cert: readFileSync('./certs/localhost.pem', 'utf-8')
-    },
+    // https: {
+    //   key: readFileSync('./certs/localhost-key.pem', 'utf-8'),
+    //   cert: readFileSync('./certs/localhost.pem', 'utf-8')
+    // },
     port: 3000
   },
   runtimeConfig: {
-    sessionSecret: process.env.NUXT_SESSION_SECRET,
     hitpayClientId: process.env.HITPAY_CLIENT_ID,
     hitpayClientSecret: process.env.HITPAY_CLIENT_SECRET,
     hitpayAuthorizeUrl: process.env.HITPAY_OAUTH_AUTHORIZE_URL,
-    hitpayTokenUrl: process.env.HITPAY_OAUTH_TOKEN_URL,
     hitpayApiBaseUrl: process.env.HITPAY_API_BASE_URL,
     hitpayRedirectUri: process.env.HITPAY_REDIRECT_URI,
     hitpayScopes: process.env.HITPAY_SCOPES || 'read_orders read_customers read_products',
